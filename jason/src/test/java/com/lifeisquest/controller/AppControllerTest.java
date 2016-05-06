d331b92c548c68782875432a0a21aef5b60dd3e8
Jason Park <jasondevelop@outlook.com>
2016-05-06 09:42:09 +0900

    Add test case.

--- jason/src/test/java/com/lifeisquest/controller/AppControllerTest.java
+++ jason/src/test/java/com/lifeisquest/controller/AppControllerTest.java
@@ -4,20 +4,30 @@ import com.lifeisquest.SpringBootPracticeApplicationTest;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.EnvironmentTestUtils;
+import org.springframework.boot.test.OutputCapture;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author Jason Park
@@ -34,14 +44,35 @@ public class AppControllerTest {
 
   private MockMvc mockMvc;
 
+  private final StandardEnvironment environment = new StandardEnvironment();
+
+  RestTemplate template = new TestRestTemplate();
+
+  @Rule
+  public OutputCapture capture = new OutputCapture();
+
+  @Rule
+  public ExpectedException expected = ExpectedException.none();
+
   @Before
   public void setUp() throws Exception {
     this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).alwaysExpect(status().isOk()).build();
+    EnvironmentTestUtils.addEnvironment(this.environment, "spring.profiles.active=prod");
   }
 
   @After
   public void tearDown() throws Exception {
+  }
 
+  @Test
+  public void getTestEnv() throws Exception {
+    System.out.println(environment.getActiveProfiles()[0]);
+  }
+
+  @Test
+  public void testName() throws Exception {
+    System.out.println("Hello World!");
+    assertThat(capture.toString(), containsString("World"));
   }
 
   @Test
@@ -54,5 +85,12 @@ public class AppControllerTest {
   public void getAppUrl() throws Exception {
     this.mockMvc.perform(get("/api/app/url").accept(MediaType.APPLICATION_JSON_UTF8).contentType(MediaType.APPLICATION_JSON_UTF8))
         .andDo(print());
+  }
+
+  @Test
+  public void testRequest() throws Exception {
+    HttpHeaders headers = template.getForEntity("http://www.naver.com", String.class).getHeaders();
+    System.out.println(headers);
+//    assertThat(headers.getLocation().toString(), containsString("myotherhost"));
   }
 }
